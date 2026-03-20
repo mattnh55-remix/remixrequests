0719a69d2a96133b2ab30380d5ecefea750e18d2
mattnh55-remix <mattnh55@gmail.com>
2026-03-20 06:00:22 -0400

    0320

---
+++ src/app/booth/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 18 }}>
+      <h1 style={{ margin: 0 }}>Remix Song Requests</h1>
+      <p style={{ opacity: 0.8 }}>
+        Try:
+        <br />/request/remixrequests
+        <br />/tv/remixrequests
+        <br />/admin/remixrequests
+      </p>
+    </div>
+  );
+}
