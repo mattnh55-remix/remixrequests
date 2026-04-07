bf3e1e4fcf1cbac4553ece8062eb2e423e40c57b
mattnh55-remix <mattnh55@gmail.com>
2026-04-07 08:47:41 -0400

    admin

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -21,7 +21,7 @@ export default function StaffLogin() {
       return;
     }
 
-    router.push("/admin/remixrequest"); // or dynamic
+    router.push("/admin/remix"); // or dynamic
   }
 
   return (
