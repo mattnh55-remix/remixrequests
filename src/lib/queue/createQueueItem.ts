64a83c92ed848dbd30e8c544fd2c03363cbfb431
mattnh55-remix <mattnh55@gmail.com>
2026-05-02 14:27:56 -0400

    queue lib

--- src/lib/queue/createQueueItem.ts
+++ src/lib/queue/createQueueItem.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "@/lib/prisma";
 
 export async function createQueueItemForApprovedRequest(requestId: string) {
   return prisma.$transaction(async (tx) => {
